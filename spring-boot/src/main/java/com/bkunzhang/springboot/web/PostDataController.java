d2c4cce15a456d342d08c1f5c11eb2f556280c2b
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2019-06-06 21:32:05 +0800

    上传文件

--- spring-boot/src/main/java/com/bkunzhang/springboot/web/PostDataController.java
+++ spring-boot/src/main/java/com/bkunzhang/springboot/web/PostDataController.java
@@ -5,9 +5,13 @@ import lombok.ToString;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by bkunzhang on 2019/6/6.
@@ -44,5 +48,17 @@ public class PostDataController {
     public String postData(@RequestParam String name, Request request) {
         System.out.println(request);
         return "Happy every day!";
+    }
+
+    @RequestMapping("/postFile")
+    public String postFile(@RequestParam("file1") MultipartFile file, @RequestParam Map<String, Object> map) throws IOException {
+        if (file.isEmpty()) {
+            return "file not find";
+        }
+        System.out.println(file.getOriginalFilename());
+        System.out.println("map=" + map);
+        String filePath = "d:/t/";
+        file.transferTo(new File(filePath + file.getOriginalFilename()));
+        return "yes";
     }
 }
