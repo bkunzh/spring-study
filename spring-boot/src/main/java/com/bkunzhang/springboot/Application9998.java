3f79610698f343611c68841f9bfe876f2c179523
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2019-09-09 15:10:48 +0800

    Application9998

---
+++ spring-boot/src/main/java/com/bkunzhang/springboot/Application9998.java
@@ -0,0 +1,15 @@
+package com.bkunzhang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application9998 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application9998.class, args);
+	}
+
+}
