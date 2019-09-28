5b7e3e790e5d849f22d8cf204fac8de1bd6f4bfd
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2019-09-28 15:05:35 +0800

    u

--- spring-boot/src/test/java/com/bkunzhang/springboot/http/HttpTest.java
+++ spring-boot/src/test/java/com/bkunzhang/springboot/http/HttpTest.java
@@ -3,22 +3,19 @@ package com.bkunzhang.springboot.http;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.ImmutableMap;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.*;
+import java.util.Map;
 
 /**
  * Created by bkunzhang on 2019/6/2.
