34bc646687730f36ea7231ca80b80534bd22cc29
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2020-03-31 22:47:44 +0800

    /file/download

--- spring-boot/src/test/java/com/bkunzhang/springboot/http/RestTemplateTest.java
+++ spring-boot/src/test/java/com/bkunzhang/springboot/http/RestTemplateTest.java
@@ -9,6 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by bkunzhang on 2019/9/3.
  */
@@ -26,5 +31,30 @@ public class RestTemplateTest {
             System.out.println(responseEntity);
             System.out.println(responseEntity.getBody());
         }
+    }
+
+    @Test
+    public void t() {
+        Map<String, Object> map = new HashMap<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        map.put("applyId", 37345);
+        map.put("yearMonthStr", sdf.format(new Date()));
+        map.put("restoreNum", 50);
+//        Map<String, Object> rs = restTemplate.getForObject("http://localhost:8080/ommall/accbookexport/restoreAccBook", Map.class, map);
+
+        Map<String, Object> rs = restTemplate.getForObject(getUrl("http://localhost:8080/ommall/accbookexport/restoreAccBook", map), Map.class);
+
+//        String url = "http://localhost:8080/ommall/accbookexport/restoreAccBook?applyId=37345&yearMonthStr=" + sdf.format(new Date()) + "&restoreNum=50";
+//        Map<String, Object> rs = restTemplate.getForObject(url, Map.class);
+        System.out.println(rs);
+    }
+
+    static String getUrl(String url, Map<String, Object> params) {
+        url += "?";
+        for (String key : params.keySet()) {
+            url += key + "=" + params.get(key) + "&";
+        }
+        url = url.substring(0, url.length() - 1);
+        return url;
     }
 }
