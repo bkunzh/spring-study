4f3d5124a42a528f6375d25a19500ec7918ad997
bkunzh <dreamkunk@foxmail.com>
2020-10-05 11:47:25 +0800

    log

--- spring-framework/src/main/java/com/bkunzh/XmlTest.java
+++ spring-framework/src/main/java/com/bkunzh/XmlTest.java
@@ -3,6 +3,7 @@ package com.bkunzh;
 import com.alibaba.fastjson.JSON;
 import com.bkunzh.dto.MyBean;
 import com.bkunzh.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,20 +13,21 @@ import java.util.Map;
  * @author bingkun_zhang
  * @date 2020/6/3
  */
+@Slf4j
 public class XmlTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         UserService userService = context.getBean(UserService.class);
-        System.out.println(userService);
-        System.out.println(userService.myService);
+        log.info("" + userService);
+        log.info("" + userService.myService);
         userService.myService.say();
 
         // env profile 实现方式1:在开发环境环境变量spring_env设置为dev，测试环境环境变量spring_env设置为test
-        System.out.println("环境变量spring_env=" + context.getEnvironment().getProperty("spring_env"));
+        log.info("" + "环境变量spring_env=" + context.getEnvironment().getProperty("spring_env"));
         MyBean myBean = context.getBean("myBean", MyBean.class);
-        System.out.println(JSON.toJSONString(myBean));
+        log.info("" + JSON.toJSONString(myBean));
 
         // 构造器声明map
-        System.out.println(JSON.toJSONString(context.getBean("mymap")));
+        log.info("" + JSON.toJSONString(context.getBean("mymap")));
     }
 }
