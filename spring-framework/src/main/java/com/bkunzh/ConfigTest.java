c758f1e79ea398e3cd648a35a0aa28f105319769
bkunzh <dreamkunk@foxmail.com>
2020-10-08 14:25:44 +0800

    spring boot profile

--- spring-framework/src/main/java/com/bkunzh/ConfigTest.java
+++ spring-framework/src/main/java/com/bkunzh/ConfigTest.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @Slf4j
 public class ConfigTest {
-    // 指定激活的环境，运行时增加jvm参数 -Dspring.profiles.active=dev
+    // 指定激活的环境，运行时增加jvm参数 -Dspring.profiles.active=dev，使用java的系统属性，java -Dkey=value xx.yy
+    // 注意spring boot中指定激活环境，java -jar xxx.jar --spring.profiles.active=dev，是用命令行参数，--server.port=8081指定端口
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         log.info("" + JSON.toJSONString(context.getEnvironment().getActiveProfiles()));
