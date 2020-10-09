24bc643b1fd4af2d402859478769361217780e67
bkunzh <dreamkunk@foxmail.com>
2020-10-09 13:10:15 +0800

    编码

--- springboot2/src/main/java/study/springboot2/Springboot2Application.java
+++ springboot2/src/main/java/study/springboot2/Springboot2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot2Application {
 
+    // --spring.profiles.active=prod 可以指定环境
     public static void main(String[] args) {
         SpringApplication.run(Springboot2Application.class, args);
     }
