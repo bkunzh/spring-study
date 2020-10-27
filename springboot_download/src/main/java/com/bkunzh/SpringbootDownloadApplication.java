b0da8aef3973412c22175cddb48385bfd9d85fff
bkunzh <dreamkunk@foxmail.com>
2020-10-27 22:42:22 +0800

    log

--- springboot_download/src/main/java/com/bkunzh/SpringbootDownloadApplication.java
+++ springboot_download/src/main/java/com/bkunzh/SpringbootDownloadApplication.java
@@ -9,6 +9,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SpringbootDownloadApplication {
 
 	public static void main(String[] args) {
+		System.out.println("开始启动");
 		SpringApplication.run(SpringbootDownloadApplication.class, args);
+		System.out.println("启动完成");
 	}
 }
